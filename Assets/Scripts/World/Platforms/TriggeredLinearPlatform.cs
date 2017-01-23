efb483af79288bad9afa3b25d71d941ae3d55f88
Dan Apperloo <Dan Apperloo>
2017-01-22 17:39:03 -0800

    Dan: Linear movement Platforms

---
+++ Assets/Scripts/World/Platforms/TriggeredLinearPlatform.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Utility.Timers;
+
+
+[System.Serializable]
+public class TriggeredLinearPlatform : LinearMovementPlatform {
+
+    public bool triggerOnTouch;
+
+    public float triggerDelay;
+
+    private bool triggeredOnTouch = false;
+    private FrameTimer timer;
+
+
+	// Use this for initialization
+	new void Start () {
+        base.Start();
+        timer = new FrameTimer(triggerDelay);
+	}
+
+	
+	// Update is called once per frame
+	new void Update () {
+        
+        if (timer.UpdateAndCheck())
+        {
+            triggeredOnTouch = true;
+        }
+
+        if (triggeredOnTouch)
+        {
+            base.Update();
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        print("OnCollisionEnter2D");
+        if (coll.gameObject.tag == "Player")
+        {
+            if (triggeredOnTouch == false)
+            {
+                timer.Start();
+            }
+        }
+    }
+}
